c51def04f5de335e823e8412cd14b93a9955ac94
Skylar Rearick <rearick.skylar@gmail.com>
2024-09-21 21:45:27 -0400

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body
         className={`${cormorantGaramond.variable} ${cabin.variable} bg-white text-black relative`}
       >
