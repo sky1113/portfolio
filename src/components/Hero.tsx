48cc8a914325c3464227fb369ac8570188001f0a
Skylar Rearick <rearick.skylar@gmail.com>
2024-09-26 13:44:42 -0400

    small changes to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,6 @@
 import { RxLinkedinLogo, RxGithubLogo } from "react-icons/rx";
+import Image from "next/image";
+import headshotImg from "@/public/me_cropped.jpeg";
 
 export default function Hero() {
   return (
@@ -6,11 +8,11 @@ export default function Hero() {
       {/* Text Content */}
       <div className="mt-8 md:mt-0 md:w-1/2">
         <h1 className="text-4xl sm:text-5xl font-bold font-serif">
-          Hi, I&apos;m <span className="text-blue-600">Skylar Rearick</span>
+          Hi, I&apos;m <span className="italic">Skylar Rearick</span>!
         </h1>
         <p className="mt-4 text-lg sm:text-xl font-sans text-gray-700">
-          I&apos;m a full-stack developer based in the Greater Seattle area,
-          specializing in building high-quality web applications.
+          I&apos;m a full-stack developer seeking roles in the Greater Seattle
+          area, specializing in building high-quality web applications.
         </p>
         <div className="flex items-center mt-6 space-x-4">
           <a
@@ -34,7 +36,7 @@ export default function Hero() {
         </div>
         <a
           href="#contact"
-          className="inline-block mt-6 px-6 py-3 bg-blue-600 text-white rounded-md text-lg font-medium hover:bg-blue-700 transition-colors duration-300"
+          className="inline-block mt-6 px-6 py-3 bg-gray-600 text-white rounded-md text-lg font-medium hover:bg-gray-700 transition-colors duration-300"
         >
           Get in Touch
         </a>
@@ -42,7 +44,9 @@ export default function Hero() {
 
       {/* Profile Image */}
       <div className="md:w-1/2 flex justify-center md:justify-end">
-        <div className="relative w-48 h-48 sm:w-56 sm:h-56 md:w-64 md:h-64 overflow-hidden rounded-full border-4 border-blue-600"></div>
+        <div className="relative w-64 h-64 sm:w-72 sm:h-72 md:w-80 md:h-80 overflow-hidden rounded-full border-4 border-gray-600 shadow-lg transition-transform duration-300 hover:scale-105">
+          <Image src={headshotImg} fill alt="Headshot of Skylar Rearick" />
+        </div>
       </div>
     </section>
   );
